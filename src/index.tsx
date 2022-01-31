c9bce86c53317e828b216991b77bc660fb7d1fac
Ben Baldwin <benbaldwin000@gmail.com>
2022-01-31 10:33:53 -0700

    snowpack setup

---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <h1>Hello, world!</h1>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
+
+// Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
+// Learn more: https://snowpack.dev/concepts/hot-module-replacement
+if (import.meta.hot) {
+  import.meta.hot.accept();
+}
