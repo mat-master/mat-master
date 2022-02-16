ebe7452ee21bf6604dd9dd47e91e0a8953daf913
Ben Baldwin <benbaldwin000@gmail.com>
2022-02-16 13:21:23 -0700

    side nav visual improvements

--- src/components/side-nav.tsx
+++ src/components/side-nav.tsx
@@ -1,12 +1,13 @@
-import { Button, Navbar } from '@mantine/core';
+import { Button, Navbar, Title } from '@mantine/core';
 import React, { ReactNode } from 'react';
 import {
 	CreditCard as BillingIcon,
 	Grid as DashboardIcon,
 	List as ClassesIcon,
+	User as AccountIcon,
 	Users as StudentsIcon,
 } from 'react-feather';
-import { NavLink, useMatch } from 'react-router-dom';
+import { Link, useMatch } from 'react-router-dom';
 
 export interface NavButtonProps {
 	label: string;
@@ -19,12 +20,13 @@ const NavButton: React.FC<NavButtonProps> = ({ label, icon, to }) => {
 
 	return (
 		<Button
-			component={NavLink}
+			component={Link}
 			to={to}
-			fullWidth
-			variant={active ? 'subtle' : 'default'}
 			leftIcon={icon}
-			sx={{ justifyContent: 'flex-start', border: 'none', marginBottom: 12 }}
+			sx={{ marginBottom: 12, border: 'none' }}
+			styles={{ inner: { justifyContent: 'left' } }}
+			variant={active ? 'light' : 'default'}
+			fullWidth
 		>
 			{label}
 		</Button>
@@ -33,12 +35,22 @@ const NavButton: React.FC<NavButtonProps> = ({ label, icon, to }) => {
 
 const SideNav: React.FC = () => {
 	return (
-		<Navbar width={{ sm: 256 }}>
+		<Navbar width={{ sm: 256 }} padding='sm'>
+			<Navbar.Section mb='md' mt='sm'>
+				<Title order={2} align='center'>
+					Mat Master
+				</Title>
+			</Navbar.Section>
+
 			<Navbar.Section>
 				<NavButton label='Dashboard' to='/' icon={<DashboardIcon size={18} />} />
 				<NavButton label='Classes' to='/classes' icon={<ClassesIcon size={18} />} />
 				<NavButton label='Students' to='/students' icon={<StudentsIcon size={18} />} />
 				<NavButton label='Billing' to='/billing' icon={<BillingIcon size={18} />} />
+			</Navbar.Section>
+
+			<Navbar.Section mt='auto'>
+				<NavButton label='John Doe' to='/account' icon={<AccountIcon size={18} />} />
 			</Navbar.Section>
 		</Navbar>
 	);
