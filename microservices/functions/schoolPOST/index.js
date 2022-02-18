fe5ced96a3201e603610459f7e1df0fd269ffbdc
Penguinz22 <nate19522@gmail.com>
2022-02-18 08:27:56 -0700

    Created universal auth bearer header implementation for microservices

--- microservices/functions/schoolPOST/index.js
+++ microservices/functions/schoolPOST/index.js
@@ -1,27 +1,12 @@
 require("/opt/nodejs/env");
 const { Client } = require('pg');
 const validator = require('validator');
-const jwt = require('jsonwebtoken');
+const auth = require('/opt/nodejs/auth');
 
 exports.handler = async (event) => {
-  if(!event.headers["Authorization"])
-    return {
-      statusCode: 401,
-      body: JSON.stringify({error: "No authorization header."})
-    };
-  const authHeader = event.headers["Authorization"];
-  if(!authHeader.startsWith("Bearer "))
-    return {
-      statusCode: 401,
-      body: JSON.stringify({error: "Invalid authorization header."})
-    };
-  const token = authHeader.substring(7);
-  const payload = await jwt.verify(token, process.env.JWTPRIVATE);
-  if(!payload)
-    return {
-      statusCode: 401,
-      body: JSON.stringify({error: "Invalid JWT."})
-    }
+  const payload = await auth.authBearer(event);
+  if(payload.statusCode)
+    return payload;
   if(!event.body) {
       return {
         statusCode: 400,
@@ -36,7 +21,7 @@ exports.handler = async (event) => {
       };
   const client = new Client();
   await client.connect();
-  const res = await client.query("INSERT INTO schools (id, name, owner, creation_date) VALUES (DEFAULT, $1, $2, DEFAULT)", [name, payload.id]);
+  const res = await client.query("INSERT INTO schools (id, name, owner, creation_date, tier) VALUES (DEFAULT, $1, $2, DEFAULT, 0)", [name, payload.id]);
   if(!res)
     return {
       statusCode: 500,
---
+++ microservices/lambda_layer/nodejs/auth.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken')
+
+export async function authBearer(event) {
+  if(!event.headers["Authorization"])
+    return {
+      statusCode: 401,
+      body: JSON.stringify({error: "No authorization header."})
+    };
+  const authHeader = event.headers["Authorization"];
+  if(!authHeader.startsWith("Bearer "))
+    return {
+      statusCode: 401,
+      body: JSON.stringify({error: "Invalid authorization header."})
+    };
+  const token = authHeader.substring(7);
+  const payload = await jwt.verify(token, process.env.JWTPRIVATE);
+  if(!payload)
+    return {
+      statusCode: 401,
+      body: JSON.stringify({error: "Invalid JWT."})
+    }
+  return payload;
+}
