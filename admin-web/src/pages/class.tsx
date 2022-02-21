83e507096033165a6d86f39978a79d6ceed7fecc
Ben Baldwin <benbaldwin000@gmail.com>
2022-02-20 23:56:02 -0700

    Added components for every page

---
+++ admin-web/src/pages/class.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import PageHeader from '../components/page-header';
+
+const ClassPage: React.FC = () => {
+	return <PageHeader title='Class' />;
+};
+
+export default ClassPage;
