def5c421aa6c23a7e91bc6ac4c9bc9aabc879f98
Ben Baldwin <benbaldwin000@gmail.com>
2022-02-24 10:13:07 -0700

    routing updates

--- admin-web/src/app.tsx
+++ admin-web/src/app.tsx
@@ -12,17 +12,23 @@ import StudentsPage from './pages/students';
 const App: React.FC = () => (
 	<BrowserRouter>
 		<Routes>
-			<Route element={<Layout />}>
-				<Route path='/' element={<DashboardPage />} />
-				<Route path='/classes' element={<ClassesPage />} />
-				<Route path='/classes/:class' element={<ClassPage />} />
-				<Route path='/students' element={<StudentsPage />} />
-				<Route path='/students/:student' element={<StudentPage />} />
-				<Route path='/billing' element={<BillingPage />} />
-			</Route>
+			<Route path='/' element={<Layout />}>
+				<Route index element={<DashboardPage />} />
 
-			<Route path='/settings' element='Settings' />
-			<Route path='/account' element='Account' />
+				<Route path='/classes'>
+					<Route index element={<ClassesPage />} />
+					<Route path=':class' element={<ClassPage />} />
+				</Route>
+
+				<Route path='/students'>
+					<Route index element={<StudentsPage />} />
+					<Route path=':student' element={<StudentPage />} />
+				</Route>
+
+				<Route path='/billing' element={<BillingPage />} />
+				<Route path='/settings' element='Settings' />
+				<Route path='/account' element='Account' />
+			</Route>
 
 			<Route path='*' element='404' />
 		</Routes>
