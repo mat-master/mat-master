e5231f133f6a5c681c0debd53840f2fb95495a14
Ben Baldwin <benbaldwin000@gmail.com>
2022-02-24 12:47:06 -0700

    basic class edit modal

---
+++ admin-web/src/components/class-edit-modal.tsx
@@ -0,0 +1,37 @@
+import { MultiSelect, Textarea, TextInput } from '@mantine/core';
+import React from 'react';
+import { memberships } from '../pages/memberships';
+import ClassScheduleInput from './class-schedule-input';
+import Modal from './modal';
+import ModalActions from './modal-actions';
+
+export interface ClassEditModalProps {
+	open: boolean;
+	onClose: VoidFunction;
+}
+
+const ClassEditModal: React.FC<ClassEditModalProps> = ({ open, onClose }) => {
+	return (
+		<Modal opened={open} onClose={onClose} title='Class Name'>
+			<TextInput label='Name' />
+
+			<Textarea label='Description' minRows={3} maxRows={8} autosize />
+
+			<MultiSelect
+				label='Memberships'
+				data={memberships.map(({ id, name }) => ({ value: id, label: name }))}
+			/>
+
+			<ClassScheduleInput />
+
+			<ModalActions
+				primaryAction={() => {}}
+				primaryLabel='Save'
+				secondaryAction={onClose}
+				secondaryLabel='Cancel'
+			/>
+		</Modal>
+	);
+};
+
+export default ClassEditModal;
