edbdca6ec8463a63e280b856c0f6b53915faa88f
Ben Baldwin <benbaldwin000@gmail.com>
2022-03-21 09:59:12 -0700

    school address input

--- products/school-web/src/components/school-modal.tsx
+++ products/school-web/src/components/school-modal.tsx
@@ -11,14 +11,6 @@ import ModalActions from './modal-actions'
 const SchoolModal: React.FC<ModalProps> = (props) => {
 	const mutation = useMutation(createSchool)
 	const form = useForm<SchoolPostBody>({
-		defaultValues: {
-			address: {
-				city: 'Fountain Hills',
-				state: 'AZ',
-				line1: '15823 E Chicory Dr',
-				postalCode: '85268',
-			},
-		},
 		resolver: yupResolver(validator.api.schoolPostSchema),
 	})
 
@@ -33,8 +25,33 @@ const SchoolModal: React.FC<ModalProps> = (props) => {
 				<Group direction='column' grow>
 					<TextInput
 						label='Name'
-						{...form.register('name')}
 						error={form.formState.errors.name?.message}
+						{...form.register('name')}
+					/>
+					<TextInput
+						label='City'
+						error={form.formState.errors.address?.city?.message}
+						{...form.register('address.city')}
+					/>
+					<TextInput
+						label='State'
+						error={form.formState.errors.address?.state?.message}
+						{...form.register('address.state')}
+					/>
+					<TextInput
+						label='Line 1'
+						error={form.formState.errors.address?.line1?.message}
+						{...form.register('address.line1')}
+					/>
+					<TextInput
+						label='Line 2'
+						error={form.formState.errors.address?.line2?.message}
+						{...form.register('address.line2')}
+					/>
+					<TextInput
+						label='Postal Code'
+						error={form.formState.errors.address?.postalCode?.message}
+						{...form.register('address.postalCode')}
 					/>
 				</Group>
 
